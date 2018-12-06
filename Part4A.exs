29896687a63b5e1f8e839aa90a7e64fb6fb4dbdf
Martina Nagy <martina@7shifts.com>
2018-12-05 23:01:41 -0600

    Part 4 Meta data

--- Part4A.exs
+++ Part4A.exs
@@ -7,6 +7,11 @@ defmodule SortData do
     [head | tail] = entries
 
     [dateDum | [info | _]] = String.split(head, "] ")
+    type = cond do
+      !String.contains?(info, "Guard #") -> :info
+      String.contains?(info, "Guard #") -> Regex.run(~r/\d+/, info)
+    end
+
     [_ | [datetime | _]] = String.split(dateDum, "[")
     [date | [time | _]] = String.split(datetime, " ")
     [year | [month | [day | _]]] = String.split(date, "-")
@@ -19,7 +24,7 @@ defmodule SortData do
     minute = String.to_integer(minute)
 
     dateObject = %DateTime{year: year, month: month, day: day, hour: hour, minute: minute, second: 00, utc_offset: -14400, std_offset: 0, time_zone: "America/Manaus", zone_abbr: "AMT"}
-    entry = %{"date" => dateObject, "info" => info, "timestamp" => dateObject |> DateTime.to_unix(:second)}
+    entry = %{"type" => type, "date" => dateObject, "info" => info, "timestamp" => dateObject |> DateTime.to_unix(:second)}
 
     accumulator = accumulator ++ [entry]
 
@@ -34,10 +39,24 @@ defmodule SortData do
   end
 end
 
+defmodule GetGuard do
+  def parse([], accumulator) do
+    accumulator
+  end
+
+  def parse(entries, accumulator) do
+    [head | tail] = entries
+    [_ | [allInfo| _]] = String.split(head["info"], "#")
+    [id | _] = String.split(allInfo, " ")
+    IO.inspect(id)
+  end
+end
+
 {:ok, entries} = File.open "Part4Adata.txt", [:read], fn(file) ->
   IO.read(file, :all)
 end
 
 split = String.split(entries, "\n")
-
-IO.inspect(SortData.parse(split, []))
+sorted = SortData.parse(split, [])
+IO.inspect(sorted)
+# IO.inspect(GetGuard.parse(sorted, []))
